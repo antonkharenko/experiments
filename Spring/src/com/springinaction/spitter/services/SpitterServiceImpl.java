b8c32cf15d4026837914b569c357846e0529ac6b
Anton Kharenko <a.kharenko@gmail.com>
2011-11-06 18:03:41 +0200

    misk

--- Spring/src/com/springinaction/spitter/services/SpitterServiceImpl.java
+++ Spring/src/com/springinaction/spitter/services/SpitterServiceImpl.java
@@ -4,6 +4,7 @@ import com.springinaction.spitter.persistence.Spitter;
 import com.springinaction.spitter.persistence.dao.SpitterDao;
 
 public class SpitterServiceImpl implements SpitterService {
+	
 	private SpitterDao spitterDao;
 	
 	public void setSpitterDao(SpitterDao spitterDao) {
