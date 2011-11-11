d182adbd02dd57e2f1329f3e786c82c4d6bbc920
Anton Kharenko <a.kharenko@gmail.com>
2011-11-11 15:54:00 +0200

    Change security service to db based

--- Spitter/db/spittle.sql
+++ Spitter/db/spittle.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `spittle` (
   `spittle_id` bigint(20) NOT NULL auto_increment,
   `spitter_id` bigint(20) default NULL,
-  `text` varchar(140) default NULL,
-  `when` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `spittle_text` varchar(140) default NULL,
+  `spittle_when` timestamp NULL default CURRENT_TIMESTAMP,
   PRIMARY KEY  (`spittle_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
--- Spitter/src/com/springinaction/spitter/persistence/dao/SpittleDao.java
+++ Spitter/src/com/springinaction/spitter/persistence/dao/SpittleDao.java
@@ -2,10 +2,13 @@ package com.springinaction.spitter.persistence.dao;
 
 import java.util.List;
 
+import org.springframework.security.access.annotation.Secured;
+
 import com.springinaction.spitter.persistence.Spittle;
 
 public interface SpittleDao {
 	
+	@Secured("ROLE_SPITTER")
 	public void saveSpittle(Spittle spittle);
 	
 	public List<Spittle> getSpittlesForSpitter(String username);
